1d1866f2a98792391488b17a73797795187ee880
Koketsoc137 <koketso.kjay@gmail.com>
2024-11-13 14:34:45 +0200

    modified atroml

---
+++ AstroMLmod.py
@@ -0,0 +1,161 @@
+import numpy as np
+
+from sklearn.neighbors import KDTree
+from sklearn.utils import check_random_state
+
+
+def two_point(data, bins, method='standard',
+              data_R=None, random_state=None):
+    """Two-point correlation function
+
+    Parameters
+    ----------
+    data : array_like
+        input data, shape = [n_samples, n_features]
+    bins : array_like
+        bins within which to compute the 2-point correlation.
+        shape = Nbins + 1
+    method : string
+        "standard" or "landy-szalay".
+    data_R : array_like (optional)
+        if specified, use this as the random comparison sample
+    random_state : integer, np.random.RandomState, or None
+        specify the random state to use for generating background
+
+    Returns
+    -------
+    corr : ndarray
+        the estimate of the correlation function within each bin
+        shape = Nbins
+    """
+    data = np.asarray(data)
+    bins = np.asarray(bins)
+    rng = check_random_state(random_state)
+
+    if method not in ['standard', 'landy-szalay']:
+        raise ValueError("method must be 'standard' or 'landy-szalay'")
+
+    if bins.ndim != 1:
+        raise ValueError("bins must be a 1D array")
+
+    if data.ndim == 1:
+        data = data[:, np.newaxis]
+    elif data.ndim != 2:
+        raise ValueError("data should be 1D or 2D")
+
+    n_samples, n_features = data.shape
+
+    # shuffle all but one axis to get background distribution
+    if data_R is None:
+        data_R = data.copy()
+        for i in range(n_features - 1):
+            rng.shuffle(data_R[:, i])
+    else:
+        data_R = np.asarray(data_R)
+        if (data_R.ndim != 2) or (data_R.shape[-1] != n_features):
+            raise ValueError('data_R must have same n_features as data')
+
+    factor = len(data_R) * 1. / len(data)
+
+    # Fast two-point correlation functions added in scikit-learn v. 0.14
+    KDT_D = KDTree(data)
+    KDT_R = KDTree(data_R)
+
+    counts_DD = KDT_D.two_point_correlation(data, bins)
+    counts_RR = KDT_R.two_point_correlation(data_R, bins)
+
+    DD = np.diff(counts_DD)
+    RR = np.diff(counts_RR)
+
+    # check for zero in the denominator
+    RR_zero = (RR == 0)
+    RR[RR_zero] = 1
+
+    if method == 'standard':
+        corr = factor ** 2 * DD / RR - 1
+    elif method == 'landy-szalay':
+        counts_DR = KDT_R.two_point_correlation(data, bins)
+
+        DR = np.diff(counts_DR)
+
+        corr = (factor ** 2 * DD - 2 * factor * DR + RR) / RR
+
+    corr[RR_zero] = np.nan
+
+    return corr
+
+
+
+def bootstrap_two_point(data, bins, Nbootstrap=10,
+                        method='standard', return_bootstraps=False,
+                        random_state=None,data_R = None):
+    """Bootstrapped two-point correlation function
+
+    Parameters
+    ----------
+    data : array_like
+        input data, shape = [n_samples, n_features]
+    bins : array_like
+        bins within which to compute the 2-point correlation.
+        shape = Nbins + 1
+    Nbootstrap : integer
+        number of bootstrap resamples to perform (default = 10)
+    method : string
+        "standard" or "landy-szalay".
+    return_bootstraps: bool
+        if True, return full bootstrapped samples
+    random_state : integer, np.random.RandomState, or None
+        specify the random state to use for generating background
+
+    Returns
+    -------
+    corr, corr_err : ndarrays
+        the estimate of the correlation function and the bootstrap
+        error within each bin. shape = Nbins
+    """
+    data = np.asarray(data)
+    bins = np.asarray(bins)
+    rng = check_random_state(random_state)
+
+    if method not in ['standard', 'landy-szalay']:
+        raise ValueError("method must be 'standard' or 'landy-szalay'")
+
+    if bins.ndim != 1:
+        raise ValueError("bins must be a 1D array")
+
+    if data.ndim == 1:
+        data = data[:, np.newaxis]
+    elif data.ndim != 2:
+        raise ValueError("data should be 1D or 2D")
+
+    if Nbootstrap < 2:
+        raise ValueError("Nbootstrap must be greater than 1")
+
+    n_samples, n_features = data.shape
+
+    # get the baseline estimate
+    corr = two_point(data, bins, method=method, random_state=rng,data_R = data_R)
+
+    bootstraps = np.zeros((Nbootstrap, len(corr)))
+
+    if data_R is not None:
+    
+        for i in range(Nbootstrap):
+            indices = rng.randint(0, n_samples, n_samples)
+            bootstraps[i] = two_point(data[indices, :], bins, method=method,
+                                      random_state=rng,data_R = data_R[indices,:])
+
+    else:
+    
+        for i in range(Nbootstrap):
+            indices = rng.randint(0, n_samples, n_samples)
+            bootstraps[i] = two_point(data[indices, :], bins, method=method,
+                                      random_state=rng)
+
+    # use masked std dev in case of NaNs
+    corr_err = np.asarray(np.ma.masked_invalid(bootstraps).std(0, ddof=1))
+
+    if return_bootstraps:
+        return corr, corr_err, bootstraps
+    else:
+        return corr, corr_err
