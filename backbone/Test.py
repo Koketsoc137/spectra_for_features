938680d30ccc09d47660b5c055fa16689b95b52e
Koketsoc137 <koketso.kjay@gmail.com>
2026-01-06 13:34:23 +0200

    halfway throuh clustering accuracy

--- backbone/Test.py
+++ backbone/Test.py
@@ -1,6 +1,8 @@
 import sys
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
+from sklearn.cluster import KMeans
+
 
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -40,10 +42,66 @@ def KNN_accuracy(rep,labels):
     return round(m_accuracy*100,2),round(var*100,2)
 
 
-def clustering_accuracy():
+def kmeans(reps, n_custers):
+        # Define the number of clusters    
+    # Create a KMeans instance with the desired number of clusters
+    kmeans = KMeans(n_clusters=n_clusters)
     
+    # Fit the model to the data
+    kmeans.fit(X)
+    
+    # Get the cluster centers and labels
+    centers = kmeans.cluster_centers_
+    labels = kmeans.labels_
+
+    return labels
+
+def clustering_accuracy(reps_ids = (all_reps,all_reps_ids),
+                        labelled = (labels,labeled_ids),
+                       n_clusters = 10):
+    """
+    1: Assign KMeans labells to source ids
+    """
+
+    k_labels = kmeans(reps = reps_ids[0], n_clusters = n_clusters)
+
+    """
+    Find the cluster assignments of the labelled subsets
+    """
+    k_labels_sub = [k_labels[all_reps_ids]]
+
+    """
+    2: For each kmeans cluster, find the most common true label
+    """
+    true_labels,labelled_ids = labelled
+
+    for cluster_id in np.unique(k_labels):
+        #obtatin the locations of all the clusters with the ids
+        cluster_mask = (k_labels == cluster_id) 
+        true_labels_in_cluster = true_labels[cluster_mask]
+
+        # Find the most common label in this cluster
+        most_common_true_label = Counter(true_labels_in_cluster).most_common(1)[0][0]
+        
+        # Count how many samples match the most common label
+        correct += np.sum(true_labels_in_cluster == most_common_label)
+
+
+    return correct/len(true_labels)
+    
+        
+    
+    accuracy= []
+    for random_state in np.random.randint(1,10000,10):
+        n_clusters = 
+                acc = sum([neigh.predict(X_test) == y_test][0])/len(y_test)
+        
+                accuracy.append(acc)
+            m_accuracy = np.mean(accuracy)
+            var = sem(accuracy)
 
     
+    return round(m_accuracy*100,2),round(var*100,2)
 
 
 
